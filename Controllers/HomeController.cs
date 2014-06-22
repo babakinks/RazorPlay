714b803c9ccb34c8ccc05f8cf5a6953a0fdaa8a9
Babak Aghili <babakaghili@icloud.com>
2014-06-21 20:16:38 -0500

    Chapter 5
    Playing with Razor stuff

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorPlay.Models;
+
+namespace RazorPlay.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one rich person",
+            Category = "Water sports",
+            Price = 257M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpressions()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
+    }
+}
